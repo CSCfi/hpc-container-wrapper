4ab6ddf6e72e6033ec3f34450c818dfa39f07818
Nortamo <40563680+Nortamo@users.noreply.github.com>
2022-03-29 10:20:39 +0000

    Modify access right before creating squashfs

--- create_inst.sh
+++ create_inst.sh
@@ -68,6 +68,10 @@ else
 fi
 # There should be a separate folder so that removal is easier
 (cd _inst_dir && rm -f _vars.sh common_functions.sh  _sing_inst_script.sh _pre_install.sh _post_install.sh $CW_TEMPLATE_SCRIPT  _extra_user_envs.sh _extra_envs.sh )
-# unable to wrap the progress bar  
+
+if [[ ! ${CW_NO_FIX_PERM+defined}  ]];then
+    print_info "Fixing permissions within squashfs image" 2
+    chmod -R ugo+rwX _inst_dir 
+fi
 mksquashfs _inst_dir/ _deploy/$CW_SQFS_IMAGE -processors $_cpus $CW_SQFS_OPTIONS 
 
