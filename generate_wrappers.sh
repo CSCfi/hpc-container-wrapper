5a9c1b83bdd87e6b96ca6c5acff789d5a9c475ac
Henrik Nortamo <40563680+Nortamo@users.noreply.github.com>
2023-01-20 15:02:00 +0200

    Update generate_wrappers.sh

--- generate_wrappers.sh
+++ generate_wrappers.sh
@@ -127,7 +127,7 @@ else
     echo "export SINGULARITY_BIND=\$SINGULARITY_BIND,\$DIR/../\$SQFS_IMAGE:\$INSTALLATION_PATH:image-src=/" >> _deploy/common.sh
 fi
 echo "if [[ \${CW_EXTRA_BIND_MOUNTS+defined} && \"$CW_ISOLATE\" == \"no\" ]]; then
-    export SINGULARITY_BIND=\$SINGULARITY_BIND,\$(echo \$CW_EXTRA_BIND_MOUNTS |  sed \"s@\$_C_DIR/\$SQFS_IMAGE:\$INSTALLATION_PATH:image-src=/@@g\")
+    export SINGULARITY_BIND=\$SINGULARITY_BIND,\$(echo \$CW_EXTRA_BIND_MOUNTS |  sed \"s@/[^:]*:\$INSTALLATION_PATH:image-src=/@@g\")
 fi" >> _deploy/common.sh
 
 
