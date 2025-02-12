61b7de200d0a7ab8f774e865980cdc26ecc9d20d
Henrik Nortamo <henrik.nortamo@csc.fi>
2025-02-12 13:49:50 +0200

    Fixes tests

--- tests/validate_slim.sh
+++ tests/validate_slim.sh
@@ -13,5 +13,5 @@ echo "pip install lxml" > extra.txt
 t_run "pip-containerize new --slim --prefix tykky_test requirements.txt" "Creating slim container works"
 t_run "singularity exec tykky_test/container.sif cat /etc/os-release  | grep 'Debian'" "Slim container is actually using debian"
 t_run "pip-containerize update --post-install extra.txt tykky_test" "Updating a slim container works"
-t_run "pip-containerize new --slim --pyver 3.13.2-slim-bullseye --prefix tykky_test2 requirements.txt | grep 'Python 3.13.2'" "--pyver flag does not break"
-t_run "tykky_test2/bin/python --version | grep 'Python 3.13.2" "Correct python version used"
+t_run "pip-containerize new --slim --pyver 3.13.2-slim-bullseye --prefix tykky_test2 requirements.txt" "--pyver flag does not break"
+t_run "tykky_test2/bin/python --version | grep 'Python 3.13.2'" "Correct python version used"
